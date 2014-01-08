5b4e29635612baec39f9bb59e71268ca5942796b
xingjianpan <xingjianpan@163.com>
2014-01-08 17:54:24 +0800

    minor

--- tests/my_unittest.py
+++ tests/my_unittest.py
@@ -1,5 +1,9 @@
 import unittest
-import text_challenge_2
+import sys
+sys.path.append("..")
+
+
+import text_challenge
 #import os.path
 #os.path.isfile(fname)
 
@@ -11,44 +15,44 @@ class TestTextVerificationChallengeFunctions(unittest.TestCase):
             
     def test_convert_pdf_to_string(self):
         target = u'U.S. Patent No. 7,010,508 (\u201cthe \u2019508 Patent\u201d; Ex. 1007) issued March 7, 2006 and is \nassigned to Landmark Technologies LLC (\u201cLandmark\u201d). Each of claims 1-17 of the \u2019508 \nPatent is unpatentable for two independent reasons. First, the specification of the \u2019508 \nPatent fails to disclose a structure for many of the means-plus-function elements in the \nclaims. Second, every element of each of those claims was known in the prior art, \nincluding the use of so-called \u201cforwardchaining\u201d \u2013 the alleged inventive feature of the \n\u2019508 Patent.\n\n\x0c'
-        content  =  text_challenge_2.convert_pdf_to_string('S1-Source.pdf')
+        content  =  text_challenge.convert_pdf_to_string('S1-Source.pdf')
         self.assertEqual(content, target)
     
     
     def test_convert_xml_to_string(self):
         target = u'\n            U.S. Patent No. 7,010,508 (\u201cthe \u2019508 Patent\u201d; Ex. 1007) issued March 7, 2006 and is assigned to Landmark Technologies LLC (\u201cLandmark\u201d). Each of claims 1-17 of the \u2019508 Patent is unpatentable for two independent reasons. First, the specification of the \u2019508 Patent fails to disclose a structure for many of the means-plus-function elements in the claims. Second, every element of each of those claims was known in the prior art, including the use of so-called \u201cforwardchaining\u201d \u2013 the alleged inventive feature of the \u2019508 Patent.'
-        content = text_challenge_2.convert_xml_to_string('S1-Target.xml')
+        content = text_challenge.convert_xml_to_string('S1-Target.xml')
         self.assertEqual(content, target)
     
     def test_convert_html_to_string(self):
         target = u'\r\nU.S.\r\nPatent No. 7,010,508 (\u201cthe \u2019508 Patent\u201d; Ex. 1007)\r\nissued March 7, 2006 and is assigned to Landmark Technologies LLC\r\n(\u201cLandmark\u201d). Each of claims 1-17 of the \u2019508\r\nPatent is unpatentable for two independent reasons. First, the\r\nspecification of the \u2019508 Patent fails to disclose a structure\r\nfor many of the means-plus-function elements in the claims. Second,\r\nevery element of each of those claims was known in the prior art,\r\nincluding the use of so-called \u201cforwardchaining\u201d \u2013\r\nthe alleged inventive feature of the \u2019508 Patent.\r\n'
-        content = text_challenge_2.convert_html_to_string('S1-Source.html')
+        content = text_challenge.convert_html_to_string('S1-Source.html')
         self.assertEqual(content, target)
     
     def test_replace_line_breaks_with_space(self):
         text = u'\r\nU.S.\r\nPatent No. 7,010,508 (\u201cthe \u2019508 Patent\u201d; Ex. 1007)\r\nissued March 7, 2006 and is assigned to Landmark Technologies LLC\r\n(\u201cLandmark\u201d). Each of claims 1-17 of the \u2019508\r\nPatent is unpatentable for two independent reasons. First, the\r\nspecification of the \u2019508 Patent fails to disclose a structure\r\nfor many of the means-plus-function elements in the claims. Second,\r\nevery element of each of those claims was known in the prior art,\r\nincluding the use of so-called \u201cforwardchaining\u201d \u2013\r\nthe alleged inventive feature of the \u2019508 Patent.\r\n'
-        processed_text = text_challenge_2.replace_line_breaks_with_space(text)
+        processed_text = text_challenge.replace_line_breaks_with_space(text)
         self.assertTrue('\n' not in processed_text and '\r\n' not in processed_text and '\r' not in processed_text)
     
     def test_replace_tabs_with_space(self):
         text = u'abc \t def \r\t ghi'
-        processed_text = text_challenge_2.replace_tabs_with_space(text)
+        processed_text = text_challenge.replace_tabs_with_space(text)
         self.assertTrue('\t' not in processed_text and '\r\t' not in processed_text)
         
         
     def test_remove_extra_space_within_sentence(self):
         text = u' abc  def   ghi '
-        processed_text = text_challenge_2.remove_extra_space_within_sentence(text)
+        processed_text = text_challenge.remove_extra_space_within_sentence(text)
         self.assertEqual('abc def ghi', processed_text)
     
     def test_remove_all_space_within_sentence(self):
         text = u' abc  def   ghi '
-        processed_text = text_challenge_2.remove_all_space_within_sentence(text)
+        processed_text = text_challenge.remove_all_space_within_sentence(text)
         self.assertEqual('abcdefghi', processed_text)
         
     def test_remove_extra_blank_lines(self):
         text = u'abc\n\ndef\n\nghi\n\n'
-        processed_text = text_challenge_2.remove_extra_blank_lines(text)
+        processed_text = text_challenge.remove_extra_blank_lines(text)
         self.assertEqual(u'abc\ndef\nghi',processed_text)
         
     def test_splitParagraphIntoSentences(self):
@@ -60,10 +64,10 @@ class TestTextVerificationChallengeFunctions(unittest.TestCase):
         text3_ = u'abc\ndef'
         text4 = u'abc!def'
         text4_ = u'abc\ndef'
-        self.assertEqual(text_challenge_2.splitParagraphIntoSentences(text1),text1_)
-        self.assertEqual(text_challenge_2.splitParagraphIntoSentences(text2),text2_)
-        self.assertEqual(text_challenge_2.splitParagraphIntoSentences(text3),text3_)
-        self.assertEqual(text_challenge_2.splitParagraphIntoSentences(text4),text4_)
+        self.assertEqual(text_challenge.splitParagraphIntoSentences(text1),text1_)
+        self.assertEqual(text_challenge.splitParagraphIntoSentences(text2),text2_)
+        self.assertEqual(text_challenge.splitParagraphIntoSentences(text3),text3_)
+        self.assertEqual(text_challenge.splitParagraphIntoSentences(text4),text4_)
 
             
     def test_re_format_text(self):
@@ -76,8 +80,8 @@ class TestTextVerificationChallengeFunctions(unittest.TestCase):
         text = u'abc123def'
         target = u'c123d'
         target2 = u'c123def'
-        self.assertEqual(text_challenge_2.find_between(text,'c','d'), target)
-        self.assertEqual(text_challenge_2.find_between(text,'c'), target2 )
+        self.assertEqual(text_challenge.find_between(text,'c','d'), target)
+        self.assertEqual(text_challenge.find_between(text,'c'), target2 )
         
   
 if __name__ == '__main__':
